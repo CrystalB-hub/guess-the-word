1d1fb6eb5610131d23c911649455524313253a0c
crystal_b <crystal.beecken@gmail.com>
2022-07-28 15:41:23 +0900

    Function to update word in progress and check for winning

--- js/script.js
+++ js/script.js
@@ -60,7 +60,43 @@ const makeGuess = function (inputLetter) {
   } else {
     guessedLetters.push(inputLetter);
     console.log(guessedLetters);
+    showGuessedLetter();
+    showWordInProgress(guessedLetters);
   }; 
+};
+
+//function to update page with player guesses
+const showGuessedLetter = function () {
+  guess.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guess.append(li);
+  }
+};
+
+//function to update word in progress
+const showWordInProgress = function(guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const showWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+    showWord.push(letter.toUpperCase());
+  } else {
+    showWord.push("●");
+  }
+}
+wordInProgress.innerText = showWord.join("");  
+checkWin();
+};
+
+//function to check winning
+const checkWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
+  }
 };
 
 
